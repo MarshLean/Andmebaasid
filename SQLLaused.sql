d1af6f096f00c3cc9dcc5520f11a667ea5661eaf
MarshLean <158562993+MarshLean@users.noreply.github.com>
2025-04-07 09:32:21 +0300

    Tabeli Loomine Ja Andmete Lisamine

--- SQLLaused.sql
+++ SQLLaused.sql
@@ -1,1 +1,35 @@
+-- SQL Server Management Stuudio
+-- Login
 
+-- (localdb)\MSSQLLocalDB
+
+--Authentification: Windows Auth - Admini Õigused LocalHostis
+
+--Authentification: SQL Server Auth - Varem Loodud Kasutajad
+
+--New Query
+
+CREATE DATABASE gerskevitsTITpv23;
+--OBJECT EXPLORER ON VAJA PIDEVALT UUENDADA KÄASITI
+
+USE gerskevitsTITpv23;
+--TABELI LOOMINE
+
+CREATE TABLE Opilane(
+OpilaneID int Primary Key identity(1,1),
+Eesnimi varchar(25),
+Perenimi varchar(30) Unique,
+Synicalaeg date,
+Aadress TEXT,
+Opilaskodu bit
+);
+SELECT * FROM Opilane;
+--TABELI KUSTUMINE
+
+DROP table Opilane;
+--ANDMETE LISAMINE TABELISSE
+
+INSERT INTO Opilane(Eesnimi, Perenimi, Synicalaeg, Aadress, Opilaskodu)
+VALUES ('Mark', 'Levin', '2000-12-5', 'Tallinn', 1),
+('Andrei', 'Astora', '897-3-6', 'DarkSouls', 0),
+('Mati', 'Kask', '2005-11-2', 'Tartu', 0);
